0ac343fe521a7e90e8558e60696feda3e137e8ef
Alx Leonar <alx.leonar@gmail.com>
2025-05-10 19:12:05 +0200

    start rework articles_reactions

--- nextera_API/nextera_blog/models/article_reactions.py
+++ nextera_API/nextera_blog/models/article_reactions.py
@@ -6,16 +6,18 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+from django.contrib.auth.models import User
+from nextera_API.nextera_blog.models import Articles, ReactionTypes
 
 class ArticleReactions(models.Model):
-    pk = models.CompositePrimaryKey('user_id', 'article_id', 'reaction_type_id')
-    user_id = models.IntegerField()
-    article_id = models.IntegerField()
-    reaction_type_id = models.IntegerField()
-    reaction_date = models.DateTimeField(blank=True, null=True)
+    pk = models.CompositePrimaryKey('user', 'article')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    reaction_type = models.ForeignKey(ReactionTypes, on_delete=models.CASCADE)
+    reaction_date = models.DateTimeField(blank=False, null=False)
+    update_date = models.DateTimeField(blank=False, null=False)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'article_reactions'
-        unique_together = (('user_id', 'article_id', 'reaction_type_id'),)
+        unique_together = (('user', 'article'),)
