db8352dbb6a328be39784b10ad04c8f240253544
Alx Leonar <alx.leonar@gmail.com>
2025-05-27 20:05:18 +0200

    add reactions to articles and comments

--- nextera_API/nextera_blog/serializers/comments_serializers.py
+++ nextera_API/nextera_blog/serializers/comments_serializers.py
@@ -2,16 +2,25 @@
 # Mandatory here
 
 from nextera_API.nextera_blog.serializers import *
+from .reaction_types_serializers import ReactionsCommentSerializer
 
 class CommentsChainSerializer(serializers.ModelSerializer):
     # Relations (use the model field name to set serialization properly)
     user = BaseAuthorsSerializer(many=False)
+    comment_reactions = serializers.SerializerMethodField()
     comment_replies = serializers.SerializerMethodField()
 
     class Meta:
         model = Comments
-        fields = ['comment_id', 'content', 'creation_date', 'update_date', 'user', 'article', 'parent_comment', 'comment_replies']
+        fields = ['comment_id', 'content', 'creation_date', 'update_date', 'user', 'article',
+                   'parent_comment', 'comment_replies', 'comment_reactions']
 
+    def get_comment_reactions(self, obj):
+        # Get all reactions
+        reaction_types = ReactionTypes.objects.all()
+        # Get counter with reactions
+        return ReactionsCommentSerializer(reaction_types, many=True, context={'comment_id': obj.comment_id}).data
+    
     def get_comment_replies(self, obj):
         children = obj.comment_replies.all()
         if children.exists():
